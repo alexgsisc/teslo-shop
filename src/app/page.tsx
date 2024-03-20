e8b821c3308ceb2cb93e06c418a7002e267fbe1b
Alexis Guadarrama <alexgsisc@gmail.com>
2024-03-19 19:10:51 -0600

    add font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { titleFont } from "@/config/fonts";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
       <h1>Hello World</h1>
+      <h1 className={`${titleFont.className} font-bold`}>Hello World</h1>
+      <h1 className={`${titleFont.className}`}>Hello World</h1>
     </main>
   );
 }
