6ca190cbfef2d30da6ffa3ab60c43e0e86a4c8e6
Alexis Guadarrama <alexgsisc@gmail.com>
2024-03-20 17:27:29 -0600

    create section products

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 export { TopMenu } from "./ui/top-menu/TopMenu";
 export { NotFoundPage } from "./ui/not-found/NotFoundPage";
-export { Title } from "./ui/title/Title"
+export { Title } from "./ui/title/Title";
+
+export { ProductGrid } from "./products/product-grid/ProductGrid";
+export { ProductGridItem } from "./products/product-grid/ProductGridItem"
