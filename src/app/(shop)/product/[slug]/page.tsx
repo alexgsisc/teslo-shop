8a6835a68d9ad87a2718029db9bab3987e19bcac
Alexis Guadarrama <alexgsisc@gmail.com>
2024-03-21 18:39:55 -0600

    product and size selector

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,51 @@
+import { SizeSelector } from "@/components";
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
 
-export default function ProductPage() {
+interface ProductPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const products = initialData.products;
+
+export default function ProductPage({ params }: ProductPageProps) {
+  const product = products.find((product) => product.slug === params.slug);
+
+  if (!product) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Hello Product Page</h1>
+    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+      {/*Slideshow */}
+      <div className="col-span-1 md:col-span-2 ">Manager</div>
+      {/*Product info */}
+      <div className="col-span-1 px-5">
+        <h1 className={` ${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+
+        <p className="text-lg mb-5">${product.price}</p>
+
+        {/* Selector de Tallas */}
+
+        <SizeSelector
+          selectedSize={product.sizes[0]}
+          availableSizes={product.sizes}
+        />
+
+        {/* Selector de Cantidad */}
+
+        {/* Button */}
+        <button className="btn-primary my-5">Agregar al carrito</button>
+
+        {/* Descripción */}
+        <h3 className="font-bold text-sm">Descripción</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </div>
   );
 }
--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -4,7 +4,7 @@ export interface Product {
   images: string[];
   inStock: number;
   price: number;
-  sizes: Sizes[];
+  sizes: Size[];
   slug: string;
   tags: string[];
   title: string;
@@ -13,5 +13,5 @@ export interface Product {
 }
 
 export type Category ="men" | "women" | "kid" | "unisex";
-export type Sizes = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
+export type Size = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
 export type Type = "shirts" | "pants" | "hoodies" | "hats";
