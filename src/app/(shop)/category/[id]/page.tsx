a648eaac6cf1f86850dc29ef8a5833c2ab7d73e8
Alexis Guadarrama <alexgsisc@gmail.com>
2024-03-21 18:06:36 -0600

    manager category

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,37 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface CategoryBiIdPageProps {
   params: {
-    id: string
-  }
+    id: Category;
+  };
 }
+const products = initialData.products;
+const labels: Record<Category, string> = {
+  men: "para hombres",
+  women: "para mujeres",
+  kid: "para niños",
+  unisex: "para todos",
+};
 
 export default function CategoryByIdPage({ params }: CategoryBiIdPageProps) {
   const { id } = params;
+  const productByCategory = products.filter((product) => product.gender === id);
 
-  if (id === 'kids') {
+  if (productByCategory.length === 0) {
     notFound();
   }
 
   return (
-    <div>
-      <h1>Hello CategoryById {id}</h1>
-    </div>
+    <>
+      <Title
+        title={`Articulos ${(labels as any)[id]}`}
+        subtitle="Todos los productos"
+        className="mb-2"
+      />
+      <ProductGrid products={productByCategory} />
+    </>
   );
 }
