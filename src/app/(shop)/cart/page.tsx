f8e7418d622304dea0ab724e6d7e830f1647249d
Alexis Guadarrama <alexgsisc@gmail.com>
2024-03-22 18:34:14 -0600

    create empty

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -10,6 +10,7 @@ const productsInCart = [
 ];
 
 export default function CartPage() {
+  // redirect('/empty');
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
       <div className="flex flex-col w-[1000px]">
