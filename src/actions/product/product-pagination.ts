aa272d6358f54ff404121e1a9e03e6a4ef8aafa4
Alexis Guadarrama <alexgsisc@gmail.com>
2024-03-27 18:55:17 -0600

    add produc pagination

--- src/actions/product/product-pagination.ts
+++ src/actions/product/product-pagination.ts
@@ -2,9 +2,16 @@
 
 import prisma from "@/lib/prisma";
 
+interface PaginationOptions {
+    page?: number
+    limit?: number
+
+}
+
 export const getPaginateProductsWithImages = async () => {
     try {
         const products = await prisma.product.findMany({
+            take: 9,
             include: {
                 ProductImage: {
                     take: 2,
@@ -14,8 +21,15 @@ export const getPaginateProductsWithImages = async () => {
                 }
             }
         })
+        console.log(products.length);
+        return {
+            products: products.map(product => ({
+                ...product,
+                images: product.ProductImage.map(image => image.url)
+            }))
+        }
         
     } catch (error) {
-
+        throw new Error('Error getting products')
     }
 }
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,10 +2,9 @@ import { getPaginateProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
 import { initialData } from "@/seed/seed";
 
-const products = initialData.products;
 
 export default  async function Home() {
-  const productsTemp = await getPaginateProductsWithImages()  
+  const {products} = await getPaginateProductsWithImages()  
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
