7caab576e52cb0681f4e562e549fafa14eb74109
Alexis Guadarrama <alexgsisc@gmail.com>
2024-04-04 18:17:51 -0600

    add action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,7 @@
 export { getPaginateProductsWithImages } from "./product/product-pagination";
 export { getProductBySlug } from "./product/get-product-by-slug";
 export { getStockBySlug } from "./product/get-stock-by-slug";
+
+// authentication
+
+export { authenticate } from "./auth/login";
